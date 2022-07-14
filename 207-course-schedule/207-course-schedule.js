d1dac7197369f080d6f94a5cc27bc4945d836d11
김민지 <minzidev@gmail.com>
2022-07-14 17:25:13 +0900

    Time: 117 ms (59.27%), Space: 48 MB (43.39%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+const buildAdjList = (n, edges) => {
+    const adjList = Array.from({ length: n }, () => []);
+    
+    for(let edge of edges) {
+        let [src, dest] = edge;
+        adjList[src].push(dest);
+    }
+    
+    return adjList;
+}
+
+const hasCycleDFS = (node, adjList, visited, arrive, depart) => {
+    arrive[node]++;
+    visited[node] = true;
+    
+    for(let neighbor of adjList[node]) {
+        if(!visited[neighbor]) {
+            visited[neighbor] = true;
+            
+            if(hasCycleDFS(neighbor, adjList, visited, arrive, depart)) return true;
+        } else {
+            if(depart[neighbor] === 0) return true;
+        }
+    }
+    
+    depart[node]++;
+    return false;
+}
+
+const canFinish = function(numCourses, prerequisites) {
+    const adjList = buildAdjList(numCourses, prerequisites);
+    const visited = {};
+    const arrive = Array.from({ length: numCourses }, () => 0);
+    const depart = Array.from({ length: numCourses }, () => 0);
+    
+    for(let vertex = 0; vertex < adjList.length; vertex++) {
+        if(!visited[vertex]) {
+            if(hasCycleDFS(vertex, adjList, visited, arrive, depart)) return false;
+        }
+    }
+    
+    return true;
+};
