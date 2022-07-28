3a0256a343b89d8f27592c81c2c372cb68cbabe8
김민지 <minzidev@gmail.com>
2022-07-28 17:27:03 +0900

    [level 2] Title: 구명보트, Time: 12.01 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -3,13 +3,13 @@
  * 2. 가장 가벼운 사람 + 가장 무거운 사람 <= 무게 제한
  * 3. 두 사람의 합이 무게 제한보다 작으면 함께 타고 구명보트 개수 추가
  * 4. 크면 가장 무거운 사람만 타고 구명보트 개수 추가
- * 5. 반복하기
+ * 5. 가장 가벼운 사람과 가장 무거운 사람이 같으면 혼자 남은 경우이므로 구명보트 개수 추가
  */
 
 function solution(people, limit) {
     let answer = 0;
     
-    // 1. people 배열 정렬
+    // 1. people 정렬
     const asc = [...people];
     asc.sort((a, b) => a - b);
     
@@ -19,14 +19,17 @@ function solution(people, limit) {
     while (start < end) {
         // 2. 가장 가벼운 사람 + 가장 무거운 사람 <= 무게 제한
         if (asc[start] + asc[end] <= limit) {
+            // 3. 두 사람 함께 타는 경우
             start++;
             end--;
             answer++;
         } else {
+            // 4. 가장 무거운 사람만 타는 경우
             end--;
             answer++;
         }
         
+        // 5. 혼자 남은 경우
         if (start === end) answer++;
     }
     
