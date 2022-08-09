b4ba7534809d4e694a0f60dca54026f14a7f98e3
김민지 <minzidev@gmail.com>
2022-08-09 19:24:02 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+    let answer = [];
+    
+    for(let x of commands) {
+        let arr = array.slice(x[0] - 1, x[1]).sort((a, b) => a - b);
+        answer.push(arr[x[2] - 1])
+        arr = [];
+    }
+    
+    return answer;
+}
