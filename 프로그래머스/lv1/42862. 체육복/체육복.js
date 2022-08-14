a79c2530ea2fde7c435606c138851fb2ac2d9375
김민지 <minzidev@gmail.com>
2022-08-15 02:19:39 +0900

    [level 1] Title: 체육복, Time: 0.13 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,24 @@
+const n = 5;
+const lost = [4, 2];
+const reserve = [3, 5];
+
+function solution(n, lost, reserve) {
+	let lostNum = lost.filter(person => !reserve.includes(person)).sort((a, b) => a - b);
+	let lostNumLen = lostNum.length;
+
+	let reserveNum = reserve.filter(person => !lost.includes(person)).sort((a, b) => a - b);
+	let reserveNumLen = reserveNum.length;
+
+	let answer = n - lostNum.length;
+
+	for (let i = 0; i < lostNumLen; i++) {
+		for (let j = 0; j < reserveNumLen; j++) {
+			if (lostNum[i] === reserveNum[j] - 1 || lostNum[i] === reserveNum[j] + 1) {
+				reserveNum.splice(j, 1);
+				answer++;
+			}
+		}
+	}
+
+	return answer;
+}
