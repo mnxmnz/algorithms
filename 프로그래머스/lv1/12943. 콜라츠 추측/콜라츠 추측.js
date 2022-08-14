897f7c936788f9c7526d2d034f6f4a9511919b34
김민지 <minzidev@gmail.com>
2022-08-15 03:03:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -2,12 +2,7 @@ function solution(num) {
 	let count = 0;
 
 	while (num !== 1 && count < 500) {
-		if (num % 2 === 0) {
-			num = num / 2;
-		} else {
-			num = num * 3 + 1;
-		}
-
+		num % 2 === 0 ? (num = num / 2) : (num = num * 3 + 1);
 		count++;
 	}
 
