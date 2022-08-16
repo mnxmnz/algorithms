a64de7f22add28e45e6d0387e8116314e7ec4b2a
김민지 <minzidev@gmail.com>
2022-08-17 01:55:05 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    const months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+    
+    let sum = 0;
+    
+    for(let i = 0; i < a - 1; i++) {
+        sum += months[i];
+    }
+    
+    answer = days[(sum += b) % 7];
+    
+    return answer;
+}
