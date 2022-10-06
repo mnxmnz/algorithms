f2e9752b09369c98a866069e3ae1789bb4a8b701
김민지 <minzidev@gmail.com>
2022-10-06 23:10:20 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,15 +1,18 @@
 function solution(fees, records) {
     const answer = [];
     
+    // 문자열 시간을 숫자로 변환된 시간으로 반환
     const getNumberTime = (time) => {
         const arr = time.split(":");
         return Number.parseInt(arr[0]) * 60 + Number.parseInt(arr[1]);
     }
     
+    // 입차 기록과 출차 기록의 시간 차이를 반환
     const getParkTime = (start, end) => { 
         return getNumberTime(end) - getNumberTime(start); 
     }
     
+    // 주차 요금 반환
     const getTotalFee = (time) => {
         // 기본 시간
         let cost = fees[1];
@@ -17,7 +20,7 @@ function solution(fees, records) {
         
         // 추가 단위 시간
         if(time > 0) {
-            cost += Math.ceil(time/fees[2]) * fees[3];
+            cost += Math.ceil(time / fees[2]) * fees[3];
         }
         
         return cost;
@@ -29,18 +32,18 @@ function solution(fees, records) {
     
     let time = 0;
     
-    for(let i = park.length-1; 0<= i;i--) {
+    for(let i = park.length - 1; 0 <= i; i--) {
         if(park[i][2] === 'IN') {
             // 나간 기록이 없는 차량
             time += getParkTime(park[i][0], "23:59");
         } else {
             // 나간 기록이 있는 차량
-            time += getParkTime(park[i-1][0], park[i][0]);
+            time += getParkTime(park[i - 1][0], park[i][0]);
             i--;
         }
         
         // 차량 종류가 바뀌면 answer 맨 앞에 주차 요금 추가
-        if(i === 0 || (park[i][1] !== park[i-1][1])) {
+        if(i === 0 || (park[i][1] !== park[i - 1][1])) {
             answer.unshift(getTotalFee(time));
             time = 0;
         }
