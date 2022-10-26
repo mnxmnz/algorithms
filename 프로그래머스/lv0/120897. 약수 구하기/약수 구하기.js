0e435a5889025b6a5113e2e95f4cbf0afe04278f
김민지 <minzidev@gmail.com>
2022-10-27 00:38:35 +0900

    [level 0] Title: 약수 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = [];
+    
+    for(let i = 1; i <= Math.sqrt(n); i++) {
+        if(n % i === 0) {
+            answer.push(i);
+            answer.push(n / i);
+        }
+    }
+    
+    return [...new Set(answer)].sort((a, b) => a - b);
+}
