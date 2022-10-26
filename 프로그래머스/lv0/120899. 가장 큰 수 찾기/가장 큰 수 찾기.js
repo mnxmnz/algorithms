e28415db5421e548e74125f3f5e2ecea1aadd6b4
김민지 <minzidev@gmail.com>
2022-10-26 20:29:58 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,13 +1,5 @@
 function solution(array) {
-    let max = 0;
-    let index = 0;
-    
-    for(let i = 0; i < array.length; i++) {
-        if(max < array[i]) {
-            max = array[i];
-            index = i;
-        } 
-    }
-    
-    return [max, index];
+    const max = Math.max(...array);
+
+    return [max, array.indexOf(max)];
 }
