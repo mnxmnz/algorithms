9576a7af93fb5987a726eb1144bb7951ac962163
김민지 <minzidev@gmail.com>
2022-11-01 00:37:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.replace(/[^0-9]/g, '').split('').reduce((a, b) => a + Number(b), 0);;
+    return my_string.replace(/[^0-9]/g, '').split('').reduce((a, b) => a + Number(b), 0);
 }
