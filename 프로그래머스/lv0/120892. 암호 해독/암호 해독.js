e56ad8086b4d3e8e2c3c724947c6508e4b8d31ff
김민지 <minzidev@gmail.com>
2022-11-17 17:04:31 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,9 +1,3 @@
 function solution(cipher, code) {
-    let answer = '';
-    
-    for(let i = code-1; i < cipher.length; i += code) {
-        answer += cipher[i];
-    }
-    
-    return answer;
+    return cipher.split('').filter((_, index) => (index + 1) % code === 0).join('');
 }
