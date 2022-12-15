e0fd0814a5f291e1ab0bed34e17c599244c7a159
김민지 <minzidev@gmail.com>
2022-12-15 20:00:48 +0900

    [level 0] Title: 369게임, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,10 +1,3 @@
 function solution(order) {
-    const orderArray = String(order).split("");
-    let answer = 0;
-    
-    for(const x of orderArray) {
-        if(x !== "0" && Number(x) % 3 === 0) answer++;
-    }
-    
-    return answer;
+    return [...order.toString().matchAll(/[3|6|9]/g)].length;
 }
