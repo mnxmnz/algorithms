acfa8c46d9a2f790f3d73cf061c917a48903a72b
김민지 <minzidev@gmail.com>
2022-12-30 01:21:47 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,13 +1,3 @@
 function solution(arr) {
-  let answer = 0;
-  let sum = 0;
-  const length = arr.length;
-
-  for (var i = 0; i < length; i++) {
-    sum += arr[i];
-  }
-
-  answer = sum / length;
-
-  return answer;
+    return arr.reduce((acc, cur) => acc += cur, 0) / arr.length;
 }
