4995461b8d5ad2b58e63b550c7fba94e704a6b8a
김민지 <minzidev@gmail.com>
2022-12-30 01:54:56 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,12 @@
 function solution(n) {
-  let answer = 0;
-
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0) answer += i;
-  }
-
-  return answer;
+    let divisor = [];
+    
+    for(let i = 1; i <= Math.sqrt(n); i++) {
+        if(n % i === 0) {
+            if(i !== n / i) divisor.push(i, n / i);
+            else divisor.push(i);
+        }
+    }
+    
+    return divisor.reduce((acc, cur) => acc + cur, 0);
 }
