35937a3b940d6581b30c0f121937515c303d0283
김민지 <minzidev@gmail.com>
2023-03-02 15:20:36 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,16 @@
+function solution(number) {
+  let answer = 0;
+  const length = number.length;
+
+  for (let i = 0; i < length - 2; i++) {
+    for (let j = i + 1; j < length - 1; j++) {
+      for (let k = j + 1; k < length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
