96f322eba83c2ccf741e911e8d1d8cc24d1d3aeb
김민지 <minzidev@gmail.com>
2023-04-02 05:20:06 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,10 +1,9 @@
 function solution(citations) {
-  const sorted = citations.sort((a, b) => b - a);
-  let index = 0;
+  citations.sort((a, b) => b - a);
 
-  while (index + 1 <= sorted[index]) {
-    index++;
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] <= i) return i;
   }
 
-  return index;
+  return citations.length;
 }
