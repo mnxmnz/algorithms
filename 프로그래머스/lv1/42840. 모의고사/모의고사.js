85504e46544093c19b0239b2c99fe6e153db3bd0
김민지 <minzidev@gmail.com>
2023-04-02 18:33:51 +0900

    [level 1] Title: 모의고사, Time: 3.47 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,22 +1,22 @@
 function solution(answers) {
-  const len = answers.length;
   const one = [1, 2, 3, 4, 5];
   const two = [2, 1, 2, 3, 2, 4, 2, 5];
   const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let count = [0, 0, 0];
 
-  let answer = [];
-  let cnt = [0, 0, 0];
-
-  for (let i = 0; i < len; i++) {
-    if (answers[i] === one[i % 5]) cnt[0]++;
-    if (answers[i] === two[i % 8]) cnt[1]++;
-    if (answers[i] === three[i % 10]) cnt[2]++;
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === one[i % one.length]) count[0]++;
+    if (answers[i] === two[i % two.length]) count[1]++;
+    if (answers[i] === three[i % three.length]) count[2]++;
   }
 
-  const max = Math.max(...cnt);
+  const max = Math.max(...count);
+  let answer = [];
 
   for (let i = 0; i < 3; i++) {
-    if (max === cnt[i]) answer.push(i + 1);
+    if (count[i] === max) {
+      answer.push(i + 1);
+    }
   }
 
   return answer;
