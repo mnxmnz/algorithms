06ee838304fce2552e77e1e4e26aa9545c002c9f
김민지 <minzidev@gmail.com>
2023-04-04 23:05:05 +0900

    [level 2] Title: 위장, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,13 +1,13 @@
 function solution(clothes) {
+  const map = new Map();
   let answer = 1;
-  let map = new Map();
 
-  clothes.forEach(cloth => {
-    map.set(cloth[1], map.get(cloth[1]) + 1 || 1);
-  });
+  for (const c of clothes) {
+    map.set(c[1], (map.get(c[1]) || 0) + 1);
+  }
 
-  for (const n of map.values()) {
-    answer *= n + 1;
+  for (let v of map.values()) {
+    answer *= v + 1;
   }
 
   return answer - 1;
