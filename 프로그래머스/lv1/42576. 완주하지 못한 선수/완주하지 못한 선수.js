467231b07fea82536e03043bf5c9e239d7bed9ee
김민지 <minzidev@gmail.com>
2023-04-04 22:48:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 28.35 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,19 @@
 function solution(participant, completion) {
   const map = new Map();
 
-  participant.forEach(name => map.set(name, map.get(name) + 1 || 1));
-  completion.forEach(name => map.set(name, map.get(name) - 1));
+  for (let i = 0; i < participant.length; i++) {
+    const p = participant[i];
+    const c = completion[i];
+
+    map.set(p, (map.get(p) || 0) + 1);
+    map.set(c, (map.get(c) || 0) - 1);
+  }
 
   for (const [k, v] of map) {
-    if (v > 0) return k;
+    if (v === 1) {
+      return k;
+    }
   }
+
+  return '';
 }
