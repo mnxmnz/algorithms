e1356153bdf3883909bec7d7ddb7ac41dfb23fb7
김민지 <minzidev@gmail.com>
2023-09-14 23:09:01 +0900

    [level 2] Title: 타겟 넘버, Time: 1.87 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,18 @@
 function solution(numbers, target) {
-  let answer = 0;
-
-  function dfs(index, sum) {
-    if (index === numbers.length) {
-      if (sum === target) {
-        answer++;
-      }
-    } else {
-      dfs(index + 1, sum + numbers[index]);
-      dfs(index + 1, sum - numbers[index]);
+    let answer = 0;
+    
+    function dfs(L, sum) {
+        if(L === numbers.length) {
+            if(sum === target) {
+                answer += 1;
+            }
+        } else {
+            dfs(L + 1, sum + numbers[L]);
+            dfs(L + 1, sum - numbers[L]);
+        }
     }
-  }
-
-  dfs(0, 0);
-
-  return answer;
+    
+    dfs(0, 0);
+    
+    return answer;
 }
